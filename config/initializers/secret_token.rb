d901a034c6c98551dcb50d982fd9e8f246eca8ad
Prashant <prashant@gyeser.com>
2013-03-18 02:25:06 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Letsdine::Application.config.secret_token = '5a143c9ee18984201e15cbe95b9847e4bee2abe28a938bbb783e79127b6a2a5b72f46321d8e20182770a9bea11ffcfd085dd227c4eb3099a375b197eab436497'
