be7adf60fe83078d87ed67a97b78c854be4dc540
Prashant <prashant@gyeser.com>
2013-03-28 15:07:14 +0530

    Added functionality to show the logged in status on the top of all the pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130327143124) do
+ActiveRecord::Schema.define(:version => 20130328075630) do
 
   create_table "attendees", :force => true do |t|
     t.integer  "user_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20130327143124) do
     t.string   "event_image"
     t.text     "invitees"
     t.integer  "user_id"
+    t.integer  "max_seats"
   end
 
   create_table "menu_items", :force => true do |t|
