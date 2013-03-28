0cd5d7b460cd2079628863822a4cca76ecaa160b
Prashant <prashant@gyeser.com>
2013-03-29 00:16:54 +0530

    Added the homepage.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
   	@home_page = true
+
+  	# Getting the last 5 event records 
+  	@events = Event.find(:all, :limit => 6).reverse
   end
 end
