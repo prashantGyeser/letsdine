b3e4f7a4db255cd91dfefd2095edfa5874594c3e
Prashant <prashant@gyeser.com>
2013-03-30 19:01:39 +0530

    Added a field for the image from facebook. Still need to show it on the pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130330073156) do
+ActiveRecord::Schema.define(:version => 20130330132054) do
 
   create_table "attendees", :force => true do |t|
     t.integer  "user_id"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(:version => 20130330073156) do
     t.string   "uid"
     t.string   "oauth_token"
     t.string   "oauth_expires_at"
+    t.string   "image"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
