67be67bf8f03f0ba41cd2067a2c1a34ca08f0bc0
Prashant <prashant@gyeser.com>
2013-04-04 22:27:22 +0530

    Modified the event_description show that it uses the truncate method instead of a custome method. The custom method was removing the spaces

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,18 +4,6 @@ class HomeController < ApplicationController
 
   	# Getting the last 6 event records 
   	@events = Event.last(8).reverse
-  	@events.each do |event| 
-  		event_description = event.event_description
-  		words = event_description.split(" ")
-  		description_summary = "" 
-  		words.each do |word|
-  			description_summary += word if description_summary.size + word.size < 100
-  		end
-
-  		description_summary += "..."
-
-  		event.event_description = description_summary
-  	end
 
   	# Getting the last four events for the slider
   	@header_events = Event.last(4)
