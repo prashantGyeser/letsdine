ec32205c441f070fef1620846f50bdbe4a31ff2a
Prashant <prashant@gyeser.com>
2013-04-16 02:08:35 +0530

    Removed the reverse from the home controller.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,10 @@ class HomeController < ApplicationController
   	@home_page = true
 
   	# Getting the last 6 event records 
-  	@events = Event.find(:all, :conditions => ["event_type != 'private'"], :limit => 8 ).reverse
+  	@events = Event.find(:all, :conditions => ["event_type != 'private'"], :limit => 8 )
   	#@events = Event.last(8).reverse
 
   	# Getting the last four events for the slider
-  	@header_events = Event.find(:all, :conditions => ["event_type != 'private'"], :limit => 4 ).reverse
+  	@header_events = Event.find(:all, :conditions => ["event_type != 'private'"], :limit => 4 )
   end
 end
