25ae71917ceaa775b75a830f98e818e5e09722dd
Prashant <prashant@gyeser.com>
2013-04-16 18:13:03 +0530

    Added a query for the homepage so that only 8 open public events are shown

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,10 @@ class HomeController < ApplicationController
   	@home_page = true
 
   	# Getting the last 6 event records 
-  	@events = Event.find(:all, :conditions => ["event_type != 'private'"], :limit => 8 )
+  	@events = Event.find(:all, :conditions => ["event_type != 'private' AND status != 'closed'"], :limit => 8 )
   	#@events = Event.last(8).reverse
+
+  	logger.debug "The events are: #{@events.inspect}"
 
   	# Getting the last four events for the slider
   	@header_events = Event.find(:all, :conditions => ["event_type != 'private'"], :limit => 4 )
