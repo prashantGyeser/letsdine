dc08d1eb2361c571aa1c191dfb1c69b73aa18cb5
Prashant <prashant@gyeser.com>
2013-06-07 15:31:48 +0530

    Fixed an issue where four other events were not being shown on a event#show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -47,15 +47,12 @@ class EventsController < ApplicationController
     event_with_token = Event.where(:token => params[:id])
     @user_invite = UserInvite.new
 
-    @other_events = Event.limit(4)
-    @other_events = @other_events.where('event_type != ?', 'private')
+    @other_events = Event.where('event_type != ?', 'private')
     @other_events = @other_events.where('status != ?', 'closed')
     
-    #@events = Event.find(:all, :conditions => ["event_type != 'private' AND status != 'closed'"], :limit => 8 )
     # Taken from http://stackoverflow.com/questions/9970300/how-to-chain-where-query-in-rails-3-active-record
     @other_events = @other_events.where('city = ?', session[:city])
-
-    
+    @other_events = @other_events.limit(4)
     
     if session[:joined]
       @just_joined = true
