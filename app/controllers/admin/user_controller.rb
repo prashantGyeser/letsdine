9d0686246a20a833780a6b653fee74fee579633a
Prashant <prashant@gyeser.com>
2013-06-25 16:39:57 +0530

    Added the number of events a user has attended to the admin/user page

--- app/controllers/admin/user_controller.rb
+++ app/controllers/admin/user_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UserController < Admin::ApplicationController
   def index
   	#@user_list = User.find(:all, :group => 'city, id, email, name')
-  	@user_list = User.all.group_by(&:city)
+  	@user_list = User.includes(:attendees).all.group_by(&:city)
   end
 end
