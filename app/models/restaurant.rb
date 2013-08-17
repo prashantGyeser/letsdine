122dfcf28f34ced827aff55953dd3a3519212735
Prashant <prashant@gyeser.com>
2013-08-17 14:24:16 +0530

    Precompiled assets and preparing to deploy to heroku

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,16 +2,17 @@
 #
 # Table name: restaurants
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  address    :string(255)
-#  cusine     :string(255)
-#  photo      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  price      :integer
-#  city       :string(255)
-#  status     :string(255)
+#  id                    :integer          not null, primary key
+#  name                  :string(255)
+#  address               :string(255)
+#  cusine                :string(255)
+#  photo                 :string(255)
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#  price                 :integer
+#  city                  :string(255)
+#  status                :string(255)
+#  commission_percentage :decimal(, )
 #
 
 class Restaurant < ActiveRecord::Base
--- spec/models/waiting_list_entry_spec.rb
+++ spec/models/waiting_list_entry_spec.rb
@@ -2,9 +2,11 @@
 #
 # Table name: waiting_list_entries
 #
-#  id         :integer          not null, primary key
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :integer          not null, primary key
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  event_id    :integer
+#  attendee_id :integer
 #
 
 require 'spec_helper'
