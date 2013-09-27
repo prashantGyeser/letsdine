675e21fe9d23e763e1f8b9df540fd1265afe04ab
Prashant <prashant@gyeser.com>
2013-09-27 15:39:03 +0530

    Added a header call to the email for signups. This is needed for Mailchimp to send out emailers

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,4 +1,6 @@
 class WelcomeMailer < ActionMailer::Base
+	after_action :set_business_headers
+
   default :from => "LetsDine <notifications@letsdine.co>"
 	def welcome(email_address, name)
 		email_subject = "Welcome to LetsDine!"
@@ -8,4 +10,11 @@ class WelcomeMailer < ActionMailer::Base
 		# Setting the headers for Mandrill tracking
 		#headers['X-MC-Track'] = "opens, clicks_htmlonly"
 	end
+
+	private
+
+	def set_business_headers
+		headers["X-MC-Tags"] = "signups"
+	end
+
 end
