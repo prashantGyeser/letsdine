720bbc70adf99395879ee27a4847f969554b5c3a
Prashant <prashant@gyeser.com>
2013-09-27 16:19:32 +0530

    fixed the mail send

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -9,7 +9,7 @@ class WelcomeMailer < ActionMailer::Base
 		#mail   to:      email_address, # normal mailer stuff
 		 #        subject: email_subject
 
-		headers['X-MC-Tags'] = "signups"
+		#headers['X-MC-Tags'] = "signups"
 		# Setting the headers for Mandrill tracking
 		#headers['X-MC-Track'] = "opens, clicks_htmlonly"
 	end
