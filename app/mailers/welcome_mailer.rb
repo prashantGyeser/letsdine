7244a2f76663f5d4db66ff58937f49060a683d50
Prashant <prashant@gyeser.com>
2013-09-27 16:08:19 +0530

    Trying to add a header to the signup emailer

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,21 +1,18 @@
 class WelcomeMailer < ActionMailer::Base
-	before_filter :set_business_headers
-  	
   	default :from => "LetsDine <notifications@letsdine.co>"
 
 	def welcome(email_address, name)
 		email_subject = "Welcome to LetsDine!"
 		@name = name
-		mail(:to => email_address, :subject => email_subject)	
+		#mail(:to => email_address, :subject => email_subject)	
 
+		mail   to:      email_address, # normal mailer stuff
+		         subject: email_subject
+
+		headers['X-MC-Tags'] = "signups"
 		# Setting the headers for Mandrill tracking
 		#headers['X-MC-Track'] = "opens, clicks_htmlonly"
 	end
 
-	private
-
-	def set_business_headers
-		headers["X-MC-Tags"] = "signups"
-	end
 
 end
