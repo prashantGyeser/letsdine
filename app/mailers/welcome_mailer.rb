0b3d510dd72dad70717a53454a42f2dc2e2a65a6
Prashant <prashant@gyeser.com>
2013-09-27 16:16:26 +0530

    Modified the way emails are sent

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -4,10 +4,10 @@ class WelcomeMailer < ActionMailer::Base
 	def welcome(email_address, name)
 		email_subject = "Welcome to LetsDine!"
 		@name = name
-		#mail(:to => email_address, :subject => email_subject)	
+		mail(:to => email_address, :subject => email_subject)	
 
-		mail   to:      email_address, # normal mailer stuff
-		         subject: email_subject
+		#mail   to:      email_address, # normal mailer stuff
+		 #        subject: email_subject
 
 		headers['X-MC-Tags'] = "signups"
 		# Setting the headers for Mandrill tracking
