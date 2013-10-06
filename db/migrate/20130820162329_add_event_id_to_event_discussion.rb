d24be8bc075bff10d11addafc9e644a5cd2c4f0b
Prashant <prashant@gyeser.com>
2013-10-07 02:47:58 +0530

    Removing the replicated event discussion migration

--- db/migrate/20130820162329_add_event_id_to_event_discussion.rb
+++
@@ -1,7 +0,0 @@
-class AddEventIdToEventDiscussion < ActiveRecord::Migration
-  def change
-  	add_column :event_discussions, :event_id, :integer
-  	add_column :event_discussions, :user_id, :integer
-  	
-  end
-end
