329145cee2228b9273983a1f8145cc8079b31220
Prashant <prashant@gyeser.com>
2013-11-05 18:38:30 +0530

    Added code to precompile all the js and css files in the assets/stylesheet and js folders.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,8 +47,9 @@ Letsdine::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += ['lib/bootstrap/bootstrap.css','lib/buttons.css']
-
+  #config.assets.precompile += ['lib/bootstrap/bootstrap.css','lib/buttons.css']
+  config.assets.precompile = ['*.js', '*.css']
+  
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
