db7a90f498f985d658960cfff308a99163860014
Prashant <prashant@gyeser.com>
2013-11-05 14:07:12 +0530

    Added a basic integration with devise for logging in. Still need to fix the redirect route once a user signs in. Right now it goes to /#/record

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -2,7 +2,32 @@
 
 var letsdineApp = angular.module('letsdineApp', ['ngResource']);
 
+// Config for signin
+letsdineApp.config(['$httpProvider', function($httpProvider){
+    $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
 
+    var interceptor = ['$location', '$rootScope', '$q', function($location, $rootScope, $q) {
+        function success(response) {
+            return response
+        };
+
+        function error(response) {
+            if (response.status == 401) {
+                $rootScope.$broadcast('event:unauthorized');
+                $location.path('/users/login');
+                return response;
+            };
+            return $q.reject(response);
+        };
+
+        return function(promise) {
+            return promise.then(success, error);
+        };
+    }];
+    $httpProvider.responseInterceptors.push(interceptor);
+}]);
+
+// Routes
 letsdineApp.config(function($routeProvider){
     $routeProvider.
         when('/',{
@@ -11,7 +36,14 @@ letsdineApp.config(function($routeProvider){
         }).
         when('/events/new', {
             controller: 'events/NewController',
-            templateUrl: '/assets/templates/events/new.html'
+            templateUrl: '/assets/events/new.html'
+        }).
+        when('/users/login', {
+            controller: 'UsersController',
+            templateUrl: '/assets/users/login.html'
+        }).
+        when('/users/register', {
+            controller: 'users/UsersController',
+            templateUrl: '/assets/users/register.html'
         })
-
 });
--- config/routes.rb
+++ config/routes.rb
@@ -59,8 +59,15 @@ Letsdine::Application.routes.draw do
   resources :event_notify_emails
 
 
-  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"} do
-    get '/users/sign_out' => 'devise/sessions#destroy'
+  #devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"} do
+  #  get '/users/sign_out' => 'devise/sessions#destroy'
+  #end
+
+  devise_for :users
+  devise_scope :user do
+    post 'login' => 'sessions#create', :as => 'login'
+    post 'logout' => 'sessions#destroy', :as => 'logout'
+    get 'current_user' => 'sessions#show_current_user', :as => 'show_current_user'
   end
 
   resources :menu_items
