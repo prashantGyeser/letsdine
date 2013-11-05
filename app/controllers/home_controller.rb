4c88960f66cf692ee263d9831e924d0a2dd09944
Prashant <prashant@gyeser.com>
2013-11-05 18:05:11 +0530

    Removed the public index file and moved back to the views/home/index file. This is taking much longer to load, need to check and see if it can be speeded up

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,15 @@
 class HomeController < ApplicationController
-  respond_to :json
+  #respond_to :json
 
   def index
-    respond_with Event.where(:event_type => 'popular')
+    #respond_with Event.where(:event_type => 'popular')
+
+    @popular_events = Event.where(:event_type => 'popular')
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @popular_events }
+    end
   end
 
 end
