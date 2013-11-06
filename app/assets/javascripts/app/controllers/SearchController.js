308a4290301c114fed639aaa44f03ea1a6cbafff
Prashant <prashant@gyeser.com>
2013-11-06 11:51:39 +0530

    Before shifting branches to add cloudfront and test and see if that speeds up the site

---
+++ app/assets/javascripts/app/controllers/SearchController.js
@@ -0,0 +1,6 @@
+'use strict';
+
+letsdineApp.controller('SearchController',
+    function SearchController($scope){
+        
+});
--- public/assets/app/directives/slider.js
+++
@@ -1,1 +0,0 @@
-"use strict";letsdineApp.directive("slider",function(){return{link:function(){}}});
