ed58e6f97d0bfaf523f6137bfa4987ec7206447b
Prashant <prashant@gyeser.com>
2013-11-08 05:34:34 +0530

    Login to the application using angularjs done. Going to try to move the calls into a service

--- app/assets/javascripts/app/controllers/UsersController.js
+++ app/assets/javascripts/app/controllers/UsersController.js
@@ -3,32 +3,65 @@
 letsdineApp.controller('UsersController',
     function UsersController($scope, $log, $http) {
 
+        $scope.login_user = {email: null, password: null};
+        $scope.login_error = {message: null, errors: {}};
+
         $scope.login = function() {
-            $http.post('../users/sign_in.json', {user: {email: $scope.user.email, password: $scope.user.password}});
+            $scope.submit({method: 'POST',
+                url: '../users/sign_in.json',
+                data: {user: {email: $scope.login_user.email, password: $scope.login_user.password}},
+                success_message: "You have been logged in.",
+                error_entity: $scope.login_error});
         };
 
         $scope.logout = function() {
-            $http({method: 'DELETE', url: '../users/sign_out.json', data: {}});
+            $scope.submit({method: 'DELETE',
+                url: '../users/sign_out.json',
+                success_message: "You have been logged out.",
+                error_entity: $scope.login_error});
         };
 
+        $scope.submit = function(parameters) {
+            $scope.reset_messages();
 
-
-        /*
-        $scope.login = function(user) {
-            $scope.authError = null;
-
-            Session.login(user.email, user.password)
-                .then(function(response) {
-                    if (!response) {
-                        $scope.authError = 'Credentials are not valid';
+            $http({method: parameters.method,
+                url: parameters.url,
+                data: parameters.data})
+                .success(function(data, status){
+                    if (status == 201 || status == 204){
+                        parameters.error_entity.message = parameters.success_message;
+                        $scope.reset_users();
                     } else {
-                        $scope.authError = 'Success!';
-                        alert("Woo hoo!");
+                        if (data.error) {
+                            parameters.error_entity.message = data.error;
+                        } else {
+                            // note that JSON.stringify is not supported in some older browsers, we're ignoring that
+                            parameters.error_entity.message = "Success, but with an unexpected success code, potentially a server error, please report via support channels as this indicates a code defect.  Server response was: " + JSON.stringify(data);
+                        }
                     }
-                }, function(response) {
-                    $scope.authError = 'Server offline, please try later';
+                })
+                .error(function(data, status){
+                    if (status == 422) {
+                        parameters.error_entity.errors = data.errors;
+                    } else {
+                        if (data.error) {
+                            parameters.error_entity.message = data.error;
+                        } else {
+                            // note that JSON.stringify is not supported in some older browsers, we're ignoring that
+                            parameters.error_entity.message = "Unexplained error, potentially a server error, please report via support channels as this indicates a code defect.  Server response was: " + JSON.stringify(data);
+                        }
+                    }
                 });
         };
-        */
+
+        $scope.reset_messages = function() {
+            $scope.login_error.message = null;
+            $scope.login_error.errors = {};
+        };
+
+        $scope.reset_users = function() {
+            $scope.login_user.email = null;
+            $scope.login_user.password = null;
+        };
 
     });
